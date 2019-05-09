60badeeecdc2d2d5c47553e0b20fdcb0cb3f115b
raz erez <razerez12@gmail.com>
2019-05-09 22:06:55 +0300

    few changes to request

--- trivia/1/LoginRequestHandler.h
+++ trivia/1/LoginRequestHandler.h
@@ -1,16 +1,19 @@
 #pragma once
 #include "IRequestHandler.h"
 #include "RequestResult.h"
+#include "RequestHandlerFactory.h"
+#include "LoginManager.h"
 
 class LoginRequestHandler:IRequestHandler
 {
 public:
-	LoginRequestHandler();
+	LoginRequestHandler(LoginManager* m_loginManager, RequestHandlerFactory* m_handlerFacroty);
 	~LoginRequestHandler();
 	bool isRequestRelevant(Request req);
 	RequestResult handleRequest(Request req);
 	RequestResult login(Request req);
 	RequestResult signup(Request req);
 private:
-
+	LoginManager* _m_loginManager;
+	RequestHandlerFactory* _m_handlerFacroty;
 };
