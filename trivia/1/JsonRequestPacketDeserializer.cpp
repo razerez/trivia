45290bd08622447d373f303482e32ef49e5e5319
raz erez <razerez12@gmail.com>
2019-05-21 15:29:37 +0300

    fex changes

--- trivia/1/JsonRequestPacketDeserializer.cpp
+++ trivia/1/JsonRequestPacketDeserializer.cpp
@@ -1,13 +1,1 @@
-#include "JsonRequestPacketDeserializer.h"
 
-LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(std::vector<char> buffer)
-{
-	LoginRequest myLogin("swe", "wewe");
-	return myLogin;
-}
-
-SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vector<char> buffer)
-{
-	SignupRequest mySignup("wef", "ewe", "we");
-	return mySignup;
-}
--- trivia/1/JsonRequestPacketDeserializer.h
+++ trivia/1/JsonRequestPacketDeserializer.h
@@ -7,8 +7,14 @@
 /*
 only here this is cant be until we build buffer.
 */
-struct JsonRequestPacketDeserializer
+LoginRequest deserializeLoginRequest(std::vector<char> buffer)
 {
-	static LoginRequest deserializeLoginRequest(std::vector<char> buffer);
-	static SignupRequest deserializeSignupRequest(std::vector<char> buffer);
-};
+	LoginRequest myLogin("swe", "wewe");
+	return myLogin;
+}
+
+SignupRequest deserializeSignupRequest(std::vector<char> buffer /*should return buffer*/)
+{
+	SignupRequest mySignup("wef", "ewe", "we");
+	return mySignup;
+}
