deafc1cab0c90d643d5745975af5ea33054c691a
Nitay <nitay1343@gmail.com>
2019-05-21 19:13:02 +0300

    change error response

--- trivia/1/JsonResponsePacketSerializer.cpp
+++ trivia/1/JsonResponsePacketSerializer.cpp
@@ -8,18 +8,6 @@ std::vector<char> JsonResponsePacketSerializer::serializeResponse(ErrorResponse 
 	std::vector<char> vec;
 	vec.push_back('e'); //first byte of the protocol
 
-	int length = err.message.length();
-	char secondsByteLength = length & 0b11111111;
-	length = length >> 8;
-	char firstByteLength = length & 0b11111111;
-
-	vec.push_back(firstByteLength);
-	vec.push_back(secondsByteLength);
-	for (int i = 0; i < err.message.length(); i++)
-	{
-		vec.push_back(err.message[i]);
-	}
-
 	return vec;
 	
 }
