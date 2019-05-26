51a2154d80ba7c318dec73b71004481e65fef5a5
raz erez <razerez12@gmail.com>
2019-05-26 11:56:12 +0300

    fixed ranTime errors in deserializeJson

--- trivia/1/JsonRequestPacketDeserializer.cpp
+++ trivia/1/JsonRequestPacketDeserializer.cpp
@@ -8,33 +8,36 @@
 #define EMAIL_SIZE_SEGMENT 1
 LoginRequest JsonRequestPacketDeserializer::deserializeLoginRequest(std::vector<char> buffer)
 {
-	int usernameSize = stoi(getBytes(CODE_SEGMENT, USERNAME_SIZE_SEGMENT, buffer));
-	int usernameKeyIndex = findStrIndex("username:", buffer, DATA_SIZE_SEGMENT);
-	int usernameIndex = findStrIndex("\"", buffer, usernameKeyIndex);
-	std::string username = getBytes(usernameIndex, usernameSize, buffer);
-
-	int passwordSize = stoi(getBytes(CODE_SEGMENT + USERNAME_SIZE_SEGMENT, PASSWORD_SIZE_SEGMENT, buffer));
-	int passwordKeyIndex = findStrIndex("password:", buffer, DATA_SIZE_SEGMENT);
-	int passwordIndex = findStrIndex("\"", buffer, passwordKeyIndex);
-	std::string password = getBytes(passwordIndex, passwordSize, buffer);
-
+	std::string username = analyzeJson(buffer, "username:", DATA_SIZE_SEGMENT, CODE_SEGMENT, USERNAME_SIZE_SEGMENT);
+	std::string password = analyzeJson(buffer, "password:", this->_dataLocation , CODE_SEGMENT + USERNAME_SIZE_SEGMENT, PASSWORD_SIZE_SEGMENT);
 	LoginRequest myLogin(username, password);
 	return myLogin;
 }
 
 SignupRequest JsonRequestPacketDeserializer::deserializeSignupRequest(std::vector<char> buffer)
 {
-	int emailSize = stoi(getBytes(CODE_SEGMENT + USERNAME_SIZE_SEGMENT + PASSWORD_SIZE_SEGMENT, EMAIL_SIZE_SEGMENT, buffer));
-	int emailKeyIndex = findStrIndex("email:", buffer, DATA_SIZE_SEGMENT);
-	int emailIndex = findStrIndex("\"", buffer, emailKeyIndex);
-	std::string email = getBytes(emailIndex, emailSize, buffer);
-
 	LoginRequest myLogin = deserializeLoginRequest(buffer);
 	std::string username = myLogin._username;
 	std::string password = myLogin._password;
+	std::string email = analyzeJson(buffer, "email:", this->_dataLocation, CODE_SEGMENT + USERNAME_SIZE_SEGMENT + PASSWORD_SIZE_SEGMENT, EMAIL_SIZE_SEGMENT);
 
 	SignupRequest mySignup(username, password, email);
 	return mySignup;
+}
+
+
+
+std::string JsonRequestPacketDeserializer::analyzeJson(std::vector<char> buffer, std::string subject, int dataLocation, int sizeLocation, int sizeLength)
+//input: buffer, keyWord to search in json, locationOfTheKeyWord, location of the size of the segment, the length of this size
+//returns analyzed string from json and insert next dataLocation into this->_dataLocation
+{
+	int size = stoi(getBytes(sizeLocation, sizeLength, buffer));
+	int keyIndex = findStrIndex(subject, buffer, dataLocation);
+	int strIndex = findStrIndex("\"", buffer, keyIndex);
+
+	std::string str = getBytes(strIndex, size, buffer);
+	this->_dataLocation = strIndex + size;
+	return str;
 }
 
 
@@ -54,5 +57,10 @@ size_t findStrIndex(std::string str, std::vector<char> buffer, int skipTo)
 	std::transform(str.begin(), str.end(), str.begin(), ::tolower);//make the str lowercase -> not case sensetive
 	std::string bufferStr(buffer.begin(), buffer.end());//crate string from vector
 	bufferStr.substr(buffer[skipTo], bufferStr.size());//cut all until startPoint
-	return bufferStr.find(str, str.size()); //return index of str in buffer// if not found return std::string::npos
+	size_t index = bufferStr.find(str, str.size());
+	if (index == std::string::npos)//string not found
+	{
+		return -1;
+	}
+	return index + skipTo; //return index of str in buffer
 }
--- trivia/1/JsonRequestPacketDeserializer.h
+++ trivia/1/JsonRequestPacketDeserializer.h
@@ -7,9 +7,13 @@
 
 struct JsonRequestPacketDeserializer
 {
-	static LoginRequest deserializeLoginRequest(std::vector<char> buffer);
-	static SignupRequest deserializeSignupRequest(std::vector<char> buffer);
+	 LoginRequest deserializeLoginRequest(std::vector<char> buffer);
+     SignupRequest deserializeSignupRequest(std::vector<char> buffer);
+private:
+	std::string analyzeJson(std::vector<char> buffer, std::string subject, int dataLocation, int sizeLocation, int sizeLength);
+	int _dataLocation;
 };
+
 std::string getBytes(int skipTo, int dataSize, std::vector<char> buffer);
 
 size_t findStrIndex(std::string str, std::vector<char> buffer, int startPoint);
