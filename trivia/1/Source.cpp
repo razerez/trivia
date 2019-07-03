fac25e6a7871d521a0c06429c9376f9c57668f1b
Nitay <nitay1343@gmail.com>
2019-07-03 19:07:54 +0300

    check json and add ','

--- trivia/1/Source.cpp
+++ trivia/1/Source.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include "SqliteDatabase.h"
 #include "Server.h"
-
+#include "JsonResponsePacketSerializer.h"
 
 int main()
 {
-
+	
 	SqliteDatabase * myDatabase = new SqliteDatabase{};
 	Server server(myDatabase);
 	server.run();
